98ab54225a261d3672d55b7e62caa1dafc476214
Nesa Abbasi <abbasi_nesa@yahoo.com>
2023-08-19 14:30:57 +0330

    Added SHA-256 to utils

---
+++ src/main/java/utils/StringSHA256.java
@@ -0,0 +1,25 @@
+package utils;
+
+import java.security.MessageDigest;
+
+public class StringSHA256 {
+
+    public static String hashString(String input) throws Exception {
+
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+        byte[] hash = md.digest(input.getBytes("UTF-8"));
+
+        // Converting byte array to Hexadecimal String
+        StringBuilder sb = new StringBuilder();
+        for (byte b : hash) {
+            sb.append(String.format("%02x", b));
+        }
+
+        String output = sb.toString();
+
+        return output;
+
+  }
+
+}
