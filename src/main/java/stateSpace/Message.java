d31a4260fc1fdc253e5471e2ca29ae2b96bda461
Hadi-loo <m.h.babalu@gmail.com>
2023-09-05 13:44:23 +0330

    Changed the structure of Message class
    Added server name, Changed how we keep parameters, and Added toString method

--- src/main/java/stateSpace/Message.java
+++ src/main/java/stateSpace/Message.java
@@ -2,23 +2,30 @@ package stateSpace;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.lang.StringBuilder;
+import dataStructure.Variable;
+import dataStructure.DiscreteVariable;
+import dataStructure.ContinuousVariable;
 
 public class Message {
 
     private String senderActor;
     private String receiverActor;
-    private Map.Entry<String, HashMap<String, Number>> message;
-    private Float arrivalTime;
+    private String serverName;            // name of the handler in the receiver actor
+    private HashMap<String, Variable> parameters;   // parameters of the handler in the receiver actor
+    private ContinuousVariable arrivalTime;
 
     public Message(
             String senderActor,
             String receiverActor,
-            Map.Entry<String, HashMap<String, Number>> message,
-            float arrivalTime
+            String serverName,
+            HashMap<String, Variable> parameters,
+            ContinuousVariable arrivalTime
     ) {
         this.senderActor = senderActor;
         this.receiverActor = receiverActor;
-        this.message = message;
+        this.serverName = serverName;
+        this.parameters = parameters;
         this.arrivalTime = arrivalTime;
     }
 
@@ -30,17 +37,36 @@ public class Message {
         return receiverActor;
     }
 
-    public Map.Entry<String, HashMap<String, Number>> getMessage() {
-        return message;
+    public String getServerName() {
+        return ServerName;
     }
 
-    public Float getArrivalTime() {
+    public HashMap<String, Variable> getParameters() {
+        return parameters;
+    }
+
+    public ContinuousVariable getArrivalTime() {
         return arrivalTime;
     }
 
-    public void setArrivalTime(float arrivalTime) {
-        if (arrivalTime >= 0) {
+    public void setArrivalTime(ContinuousVariable arrivalTime) {
+        if (arrivalTime.isValid()) {
             this.arrivalTime = arrivalTime;
         }
     }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Sender Actor: ").append(getSenderActor()).append("\n");
+        stringBuilder.append("Receiver Actor: ").append(getReceiverActor()).append("\n");
+        stringBuilder.append("Server Name: ").append(getServerName()).append("\n");
+        stringBuilder.append("Arrival Time: ").append(getArrivalTime().toString()).append("\n");
+        stringBuilder.append("Parameters: ").append("\n");
+        for (Map.Entry<String, Variable> entry : getParameters().entrySet()) {
+            stringBuilder.append(entry.getKey()).append(": ").append(entry.getValue().toString()).append("\n");
+        }
+        return stringBuilder.toString();
+    }
+
 }
