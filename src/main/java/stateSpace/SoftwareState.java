3eeb986e5adf0edab0173875c462cac3615d44a8
Hadi-loo <m.h.babalu@gmail.com>
2023-11-13 10:14:04 +0330

    Added takeMessage method

--- src/main/java/stateSpace/SoftwareState.java
+++ src/main/java/stateSpace/SoftwareState.java
@@ -2,14 +2,14 @@ package stateSpace;
 
 import dataStructure.DiscreteVariable;
 import dataStructure.ContinuousVariable;
+import dataStructure.Variable;
 import org.rebecalang.compiler.modelcompiler.corerebeca.objectmodel.Statement;
+import utils.CompilerUtil;
 
 import javax.annotation.Nonnull;
 import java.lang.StringBuilder;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Queue;
+import java.math.BigDecimal;
+import java.util.*;
 
 public class SoftwareState extends ActorState {
 
@@ -75,11 +75,75 @@ public class SoftwareState extends ActorState {
 
     public boolean messageCanBeTaken(ContinuousVariable globalTime) {
         for (Message message : getQueue()) {
-            if (message.messageCheckBounds(globalTime)) {
+            if (message.checkBounds(globalTime)) {
                 return true;
             }
         }
         return false;
+    }
+
+    @Override
+    public List<ActorState> takeMessage(ContinuousVariable globalTime) {
+        List<ActorState> result = new ArrayList<>();
+        SoftwareState newSoftwareState = new SoftwareState(
+                getActorName(),
+                getDiscreteVariableValuation(),
+                getQueue(),
+                getSigma(),
+                getLocalTime(),
+                getResumeTime()
+        );
+        // TODO: !!!START FROM HERE!!!
+        // FIXME: what should we do if we have more than one message that can be taken?
+        Message message = newSoftwareState.queue.poll();
+        BigDecimal tMin = globalTime.getUpperBound().min(message.getArrivalTime().getUpperBound());
+        // TODO: update sigma(not the sigma in code, the evaluation function which is sigma in paper)
+        // TODO: add message's parameter evaluation to discrete variable valuation (which is sigma in paper)
+        // FIXME: message parameters are variables, not discrete variables. what should we about intervals?
+        Map<String, Variable> parameters = message.getParameters();
+        for (Map.Entry<String, Variable> entry : parameters.entrySet()) {
+            if (entry.getValue() instanceof DiscreteVariable) {
+                newSoftwareState.getDiscreteVariableValuation().put(entry.getKey(), (DiscreteVariable) entry.getValue());
+            }
+        }
+        // TODO: remove the proper message from message bag
+        // TODO: add body of the message to list of statement to be executed
+        // FIXME: implement getMessageBody method
+        CompilerUtil.getMessageBody(newSoftwareState.getActorName(), message.getServerName());
+        // TODO: update resume time
+        // CHECKME: why we should update resume time?
+        // FIXME: what should be the name of the ContinuousVariable?
+        this.setResumeTime(new ContinuousVariable("resumeTime", globalTime.getLowerBound(), globalTime.getUpperBound()));
+
+        result.add(newSoftwareState);
+
+        // CHECKME: shouldn't it be <= instead of <?
+        if (globalTime.getUpperBound().compareTo(message.getArrivalTime().getUpperBound()) < 0) {
+            newSoftwareState = new SoftwareState(
+                    getActorName(),
+                    getDiscreteVariableValuation(),
+                    getQueue(),
+                    getSigma(),
+                    getLocalTime(),
+                    getResumeTime()
+            );
+            message = newSoftwareState.queue.poll();
+            Message newMessage = new Message(
+                    message.getSenderActor(),
+                    message.getReceiverActor(),
+                    message.getServerName(),
+                    message.getParameters(),
+                    new ContinuousVariable("arrivalTime", globalTime.getUpperBound(), message.getArrivalTime().getUpperBound())
+            );
+            newSoftwareState.addMessage(newMessage);
+            // FIXME: what epsilon means for sigma? should we set it to null? or should we set it to empty list?
+            newSoftwareState.setSigma(new ArrayList<>());
+            // FIXME: what epsilon means for resumeTime?
+            newSoftwareState.setResumeTime(new ContinuousVariable("resumeTime", globalTime.getLowerBound(), globalTime.getUpperBound()));
+
+        }
+
+        return result;
     }
 
 }
