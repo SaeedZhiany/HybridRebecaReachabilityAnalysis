50e27eb1cd8e61252ac4e913569115a146410a67
Nesa Abbasi <abbasi_nesa@yahoo.com>
2024-04-23 16:41:48 +0330

    chore: Added Visitor classes

---
+++ src/main/java/visitors/IVisitor.java
@@ -0,0 +1,14 @@
+package visitors;
+
+import org.rebecalang.compiler.modelcompiler.corerebeca.objectmodel.*;
+import stateSpace.HybridState;
+
+public interface IVisitor<T> {
+
+    public T visit(DotPrimary dotPrimary);
+    public T visit(UnaryExpression unaryExpression);
+    public T visit(BinaryExpression binaryExpression);
+    public T visit(TermPrimary termPrimary);
+    public T visit(Literal literal);
+
+}
---
+++ src/main/java/visitors/Visitor.java
@@ -0,0 +1,31 @@
+package visitors;
+
+import org.rebecalang.compiler.modelcompiler.corerebeca.objectmodel.*;
+
+public class Visitor<T> implements IVisitor<T>{
+
+    @Override
+    public T visit(DotPrimary dotPrimary) {
+        return null;
+    }
+
+    @Override
+    public T visit(UnaryExpression unaryExpression) {
+        return null;
+    }
+
+    @Override
+    public T visit(BinaryExpression binaryExpression) {
+        return null;
+    }
+
+    @Override
+    public T visit(TermPrimary termPrimary) {
+        return null;
+    }
+
+    @Override
+    public T visit(Literal literal) {
+        return null;
+    }
+}
