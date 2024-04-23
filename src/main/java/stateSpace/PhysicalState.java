94f32dafd7115e973e3a7bd46801e56ca15fe4b3
Nesa Abbasi <abbasi_nesa@yahoo.com>
2024-04-23 16:31:38 +0330

    feat: Added takeMessage method to physical state

--- src/main/java/stateSpace/PhysicalState.java
+++ src/main/java/stateSpace/PhysicalState.java
@@ -1,44 +1,67 @@
 package stateSpace;
 
-import dataStructure.ContinuousVariable;
-import dataStructure.DiscreteVariable;
+import com.rits.cloning.Cloner;
+import dataStructure.*;
 import org.rebecalang.compiler.modelcompiler.corerebeca.objectmodel.Statement;
+import utils.CompilerUtil;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import java.util.HashMap;
+import java.util.*;
 import java.lang.StringBuilder;
-import java.util.List;
-import java.util.Map;
-import java.util.Queue;
 
 public class PhysicalState extends ActorState {
 
     /**
-     * key: variable name
-     * value: variable current value
+     * mode of actor
+     * the none mode represents as "none"
      */
-    @Nonnull
-    protected HashMap<String, ContinuousVariable> continuousVariableValuation;
 
-    /**
-     * current mode of physical actor
-     */
     @Nullable
     private String mode;
 
     public PhysicalState(
             @Nonnull String actorName,
             @Nullable String mode,
-            @Nonnull HashMap<String, DiscreteVariable> discreteVariableValuation,
-            @Nonnull HashMap<String, ContinuousVariable> continuousVariableValuation,
-            @Nonnull Queue<Message> queue,
+            @Nonnull HashMap<String, Variable> variableValuation,
+            @Nonnull Set<Message> messageBag,
             @Nonnull List<Statement> sigma,
             float localTime
     ) {
-        super(actorName, discreteVariableValuation, queue, sigma, localTime);
+        super(actorName, variableValuation, messageBag, sigma, localTime);
         this.mode = mode;
-        this.continuousVariableValuation = continuousVariableValuation;
+    }
+
+    public PhysicalState(PhysicalState physicalState) {
+        super("init", new HashMap<>(), new HashSet<>(), new ArrayList<>(), 0);
+        this.actorName = physicalState.getActorName();
+        this.mode = physicalState.getMode();
+        HashMap<String, Variable> newVariableValuation = new HashMap<>();
+        for (Map.Entry<String, Variable> entry : physicalState.getVariableValuation().entrySet()) {
+            if (entry.getValue() instanceof DiscreteDecimalVariable) {
+                newVariableValuation.put(entry.getKey(), new DiscreteDecimalVariable((DiscreteDecimalVariable) entry.getValue()));
+            } else if (entry.getValue() instanceof ContinuousVariable) {
+                newVariableValuation.put(entry.getKey(), new ContinuousVariable((ContinuousVariable) entry.getValue()));
+            } else if (entry.getValue() instanceof IntervalRealVariable) {
+                newVariableValuation.put(entry.getKey(), new IntervalRealVariable((IntervalRealVariable) entry.getValue()));
+            } else if (entry.getValue() instanceof DiscreteBoolVariable) {
+                newVariableValuation.put(entry.getKey(), new DiscreteBoolVariable((DiscreteBoolVariable) entry.getValue()));
+            }
+        }
+        Set<Message> newMessageBag = new HashSet<>();
+        for (Message message : physicalState.getMessageBag()) {
+            newMessageBag.add(new Message(message));
+        }
+        this.messageBag = newMessageBag;
+        List<Statement> newSigma = new ArrayList<>();
+        Cloner cloner = new Cloner();
+        for (Statement statement : physicalState.getSigma()) {
+            // FIXME: this is a shallow copy, should we use a deep copy?
+            Statement copiedStatement = cloner.deepClone(statement);
+            newSigma.add(copiedStatement);
+        }
+        this.sigma = newSigma;
+        this.localTime = physicalState.getLocalTime();
     }
 
     @Nullable
@@ -50,51 +73,104 @@ public class PhysicalState extends ActorState {
         this.mode = mode;
     }
 
-    @Nonnull
-    public HashMap<String, ContinuousVariable> getContinuousVariableValuation() {
-        return continuousVariableValuation;
-    }
-
-    @Nullable
-    public ContinuousVariable getContinuousVariable(String name) {
-        return this.continuousVariableValuation.get(name);
-    }
-
-    public void updateContinuousVariable(ContinuousVariable continuousVariable) {
-        this.continuousVariableValuation.put(continuousVariable.getName(), continuousVariable);
+    public boolean isIdle() {
+        return mode.equals("none");
     }
 
     @Override
     public String toString() {
+        // CHECKME: which variables should be included in the string?
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("Actor: ").append(actorName).append("\n");
-        stringBuilder.append("Mode: ").append(mode).append("\n");
-        stringBuilder.append("Discrete Variables: ").append("\n");
+        stringBuilder.append("Actor: ").append(getActorName()).append("\n");
+        stringBuilder.append("Mode: ").append(getMode()).append("\n");
+        stringBuilder.append("Local Time: ").append(getLocalTime()).append("\n");
+
+        stringBuilder.append("Variable Valuation: ").append("\n");
         // CHECKME: order of the variables is not guaranteed, is it a problem?
-        for (Map.Entry<String, DiscreteVariable> entry : discreteVariableValuation.entrySet()) {
+        for (Map.Entry<String, Variable> entry : getVariableValuation().entrySet()) {
             stringBuilder.append(entry.getKey()).append(": ").append(entry.getValue().toString()).append("\n");
         }
 
-        stringBuilder.append("Continuous Variables: ").append("\n");
-        // CHECKME: order of the variables is not guaranteed, is it a problem?
-        for (Map.Entry<String, ContinuousVariable> entry : continuousVariableValuation.entrySet()) {
-            stringBuilder.append(entry.getKey()).append(": ").append(entry.getValue().toString()).append("\n");
-        }
-
-        stringBuilder.append("Queue: ").append("\n");
+        stringBuilder.append("Message Bag: ").append("\n");
         // CHECKME: order of the messages is not guaranteed, is it a problem?
-        for (Message message : queue) {
+        for (Message message : getMessageBag()) {
             stringBuilder.append(message.toString()).append("\n");
         }
 
         stringBuilder.append("Sigma: ").append("\n");
         // CHECKME: order of the statements is not guaranteed, is it a problem?
-        for (Statement statement : sigma) {
+        for (Statement statement : getSigma()) {
             stringBuilder.append(statement.toString()).append("\n");
         }
 
-        stringBuilder.append("Local Time: ").append(localTime).append("\n");
-        
         return stringBuilder.toString();
+    }
+
+    public boolean messageCanBeTaken(ContinuousVariable globalTime) {
+        for (Message message : messageBag) {
+            if (message.checkBounds(globalTime)) {
+                if (!message.getServerName().equals("setMode")) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public List<Message> getMessagesToBeTaken(ContinuousVariable globalTime) {
+        List<Message> result = new ArrayList<>();
+        for (Message message : getMessageBag()) {
+            if (message.checkBounds(globalTime) && !message.getServerName().equals("setMode")) {
+                result.add(message);
+            }
+        }
+        return result;
+    }
+
+    public List<ActorState> takeMessage(ContinuousVariable globalTime) {
+        /**
+         * TODO: START FROM HERE
+         */
+        List<ActorState> result = new ArrayList<>();
+        List<Message> messagesToBeTaken = getMessagesToBeTaken(globalTime);
+        Cloner cloner = new Cloner();
+        for (Message message : messagesToBeTaken) {
+            PhysicalState newPhysicalState = cloner.deepClone(this);
+            // TODO: !!!START FROM HERE!!!
+//            BigDecimal tMin = globalTime.getUpperBound().min(message.getArrivalTime().getUpperBound());
+            // updating actor valuation function
+            // CHECKME: what should we do if parameters have same name as a valuation variable? we are overwriting them here
+            // CHECKME: shouldn't we get a copy of parameters and then add them to variable valuation? (to avoid overwriting)
+            newPhysicalState.addVariables(message.getParameters());
+            // removing message from message bag
+            newPhysicalState.removeMessage(message);
+            // TODO: add body of the message to list of statement to be executed
+            List<Statement> messageBody = CompilerUtil.getMessageBody(newPhysicalState.getActorName(), message.getServerName());
+            newPhysicalState.addStatements(messageBody);
+            // TODO: update resume time
+            // CHECKME: why we should update resume time?
+            // FIXME: what should be the name of the ContinuousVariable?
+            result.add(newPhysicalState);
+
+            // CHECKME: shouldn't it be <= instead of <?
+            if (globalTime.getUpperBound().compareTo(message.getArrivalTime().getUpperBound()) < 0) {
+                newPhysicalState = cloner.deepClone(this);
+                Message newMessage = new Message(
+                        message.getSenderActor(),
+                        message.getReceiverActor(),
+                        message.getServerName(),
+                        message.getParameters(),
+                        new ContinuousVariable("arrivalTime", globalTime.getUpperBound(), message.getArrivalTime().getUpperBound())
+                );
+                newPhysicalState.removeMessage(message);
+                newPhysicalState.addMessage(newMessage);
+                // FIXME: what epsilon means for sigma? should we set it to null? or should we set it to empty list?
+                newPhysicalState.setSigma(new ArrayList<>());
+                // FIXME: what epsilon means for resumeTime?
+                result.add(newPhysicalState);
+            }
+        }
+
+        return result;
     }
 }
