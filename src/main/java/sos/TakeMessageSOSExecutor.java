4ced6da45f1d68c194457b86cb32493002c6c5dd
Nesa Abbasi <abbasi_nesa@yahoo.com>
2024-04-23 16:35:47 +0330

    chore: Added some methods

--- src/main/java/sos/TakeMessageSOSExecutor.java
+++ src/main/java/sos/TakeMessageSOSExecutor.java
@@ -16,13 +16,22 @@ public class TakeMessageSOSExecutor extends AbstractSOSExecutor {
         for (SoftwareState softwareState : hybridState.getSoftwareStates().values()) {
             if (!softwareState.hasStatement()) {
                 if (!hybridState.isSuspended(softwareState.getResumeTime())) {
-                    if (!softwareState.messageCanBeTaken(hybridState.getGlobalTime())) {
+                    if (softwareState.messageCanBeTaken(hybridState.getGlobalTime())) {
                         return true;
                     }
                 } 
             }
         }
         // FIXME: do this for physical states
+        for (PhysicalState physicalState : hybridState.getPhysicalStates().values()) {
+            if (!physicalState.hasStatement()) {
+                if (!physicalState.isIdle()) {
+                    if (physicalState.messageCanBeTaken(hybridState.getGlobalTime())) {
+                        return true;
+                    }
+                }
+            }
+        }
         return false;
     }
     
@@ -31,22 +40,30 @@ public class TakeMessageSOSExecutor extends AbstractSOSExecutor {
     @Override
     protected List<HybridState> execute(HybridState hybridState) {
         List<HybridState> result = new ArrayList<>();
-        for (SoftwareState softwareState : applicableStates(hybridState)) {
+        for (SoftwareState softwareState : applicableSoftwareStates(hybridState)) {
             // TODO: takeMessage method of HybridState can and should return multiple (at most 2?!) newHybridStates
             List<ActorState> generatedActorStates = hybridState.takeMessage(softwareState);
             for (ActorState actorState : generatedActorStates) {
                 // FIXME: make a copy of this software state
-                HybridState newHybridState = hybridState.copy();
+                HybridState newHybridState = new HybridState(hybridState);
                 newHybridState.replaceActorState(actorState);
                 result.add(newHybridState);
             }
         }
         // TODO: do the same thing for physical states
+        for (PhysicalState physicalState : applicablePhysicalStates(hybridState)) {
+            List<ActorState> generatedActorStates = hybridState.takeMessage(physicalState);
+            for (ActorState actorState : generatedActorStates) {
+                HybridState newHybridState = new HybridState(hybridState);
+                newHybridState.replaceActorState(actorState);
+                result.add(newHybridState);
+            }
+        }
         return result;
     }
 
     @Nonnull
-    private List<SoftwareState> applicableStates(HybridState hybridState) {
+    private List<SoftwareState> applicableSoftwareStates(HybridState hybridState) {
         List<SoftwareState> applicableStates = new ArrayList<>();
         for (SoftwareState softwareState : hybridState.getSoftwareStates().values()) {
             if (!softwareState.hasStatement()) {
@@ -60,4 +77,18 @@ public class TakeMessageSOSExecutor extends AbstractSOSExecutor {
         return applicableStates;
     }
 
+    @Nonnull
+    private List<PhysicalState> applicablePhysicalStates(HybridState hybridState) {
+        List<PhysicalState> applicableStates = new ArrayList<>();
+        for (PhysicalState physicalState : hybridState.getPhysicalStates().values()) {
+            if (!physicalState.hasStatement()) {
+                if (!physicalState.isIdle()) {
+                    if (!physicalState.messageCanBeTaken(hybridState.getGlobalTime())) {
+                        applicableStates.add(physicalState);
+                    }
+                }
+            }
+        }
+        return applicableStates;
+    }
 }
