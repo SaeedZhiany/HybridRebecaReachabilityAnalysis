3a39d08852c7c22993bf5d0a9d0069b488e9de03
ali <ali.n.hodaei@gmail.com>
2024-07-13 18:36:26 +0330

    feat: delay statement executor added

--- src/main/java/visitors/DelayApplicableVisitor.java
+++ src/main/java/visitors/DelayApplicableVisitor.java
@@ -1,12 +1,8 @@
 package visitors;
 
-import org.rebecalang.compiler.modelcompiler.corerebeca.objectmodel.BinaryExpression;
-import org.rebecalang.compiler.modelcompiler.corerebeca.objectmodel.DotPrimary;
 import org.rebecalang.compiler.modelcompiler.corerebeca.objectmodel.Statement;
 import org.rebecalang.compiler.modelcompiler.corerebeca.objectmodel.TermPrimary;
 import stateSpace.ActorState;
-
-import java.util.List;
 
 public class DelayApplicableVisitor extends Visitor<Boolean> {
     public Boolean checkStatementApplicability(ActorState actorState) {
