b520c57a141181496ff7db68b907aeac34a5455d
ali <ali.n.hodaei@gmail.com>
2024-09-04 15:44:59 +0330

    feat: add abilty to retrieve last states of NonTimeProgressExecutor or all

--- src/main/java/sos/NonTimeProgressSOSExecutor.java
+++ src/main/java/sos/NonTimeProgressSOSExecutor.java
@@ -18,8 +18,9 @@ public class NonTimeProgressSOSExecutor {
         add(new IfStatementSOSExecutor());
     }};
 
-    public List<HybridState> generateNextStates(HybridState hybridState) {
+    public List<HybridState> generateNextStates(HybridState hybridState, boolean returnTotalStates) {
         final Map<String, HybridState> totalStates = new HashMap<>();
+        final Map<String, HybridState> lastStateResults = new HashMap<>();
         Map<String, HybridState> nextRoundStates = new HashMap<>();
         Map<String, HybridState> currentRoundStates = new HashMap<>();
 
@@ -28,22 +29,35 @@ public class NonTimeProgressSOSExecutor {
         boolean isProcessEnd = false;
         while (!isProcessEnd) {
             isProcessEnd = true;
-            for (AbstractSOSExecutor executor : sosExecutors) {
-                for (Map.Entry<String, HybridState> entry : currentRoundStates.entrySet()) {
+            for (Map.Entry<String, HybridState> entry : currentRoundStates.entrySet()) {
+                boolean isStateProcessEnd = true;
+                for (AbstractSOSExecutor executor : sosExecutors) {
                     if (executor.isApplicable(entry.getValue())) {
                         isProcessEnd = false;
+                        isStateProcessEnd = false;
                         List<HybridState> generatedHybridStates = executor.execute(entry.getValue());
                         for (HybridState generatedHybridState : generatedHybridStates) {
                             generatedHybridState.updateHash();
+                            generatedHybridState.setParentHash(entry.getKey());
                             nextRoundStates.put(generatedHybridState.getHash(), generatedHybridState);
                         }
                     }
+                }
+                if (isStateProcessEnd) {
+                    lastStateResults.put(entry.getKey(), entry.getValue());
                 }
             }
             totalStates.putAll(currentRoundStates);
             currentRoundStates = new HashMap<>(nextRoundStates);
             nextRoundStates.clear();
         }
-        return new ArrayList<>(totalStates.values());
+        if (returnTotalStates)
+            return new ArrayList<>(totalStates.values());
+
+        for (Map.Entry<String, HybridState> entry : lastStateResults.entrySet()) {
+            if (!entry.getValue().getHash().equals(hybridState.getHash()))
+                entry.getValue().setParentHash(hybridState.getHash());
+        }
+        return new ArrayList<>(lastStateResults.values());
     }
 }
